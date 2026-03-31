d7f5f9da6659748d5005db12ad12f8bdf5b75f54
fabio-pastore <fabio.pastoree@gmail.com>
2026-03-31 23:06:12 +0200

    created WebParser class Co-authored-by: Alessandro <alessandro@zannone.dev> Co-authored-by: Yihao <quelwu24@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,81 +1,13 @@
 import asyncio
-from bs4 import BeautifulSoup
-from crawl4ai import AsyncWebCrawler, CrawlResult, CrawlerRunConfig, DefaultMarkdownGenerator, PruningContentFilter, CacheMode, BrowserConfig
-import string
-import regex as re
+from parser.WebParser import WebParser
 
 async def main():
-    browser_cfg : BrowserConfig = BrowserConfig(headless=True)
+    myParser : WebParser = WebParser()
+    data: dict[str, str] = await myParser.parse_url(url='https://en.wikipedia.org/wiki/Among_Us')
 
-    # #See_also, #Notes, #References, #External_links, 
-    # #Voci_correlate, #Note, #Collegamenti_esterni .portal-bar
-    css_exclusions = '''
-    #mw-head, #mw-panel, #footer, #vector-main-menu, .mw-content-subtitle,
-    .vector-header-container, .vector-column-start, .shortdescription, 
-    .vector-sticky-header, .mw-footer, .vector-sitenotice-container, 
-    .reflist, .refbegin, .mw-references-wrap, .infobox, .mw-file-description,
-    .thumb, .mw-editsection, .navbox, .side-box, .hatnote[role="complementary"], 
-    .floatright''' # add .wikitable if too much useless data is parsed 
-    # TODO: modify to include only Italian exclusions
-
-    targets = ['.mw-parser-output']
-    tag_exclusions = ['nav', 'footer', 'aside', 'script', 'style', 'noscript', 'header', 'figure']
-    markdown_gen_options = {
-        'ignore_images': True, 
-        'escape_html': True, 
-        'ignore_links': True 
-    }
-
-    crawler_cfg : CrawlerRunConfig = CrawlerRunConfig(target_elements=targets, excluded_tags=tag_exclusions, 
-                                                      markdown_generator= DefaultMarkdownGenerator(PruningContentFilter(), 
-                                                                        options=markdown_gen_options),
-                                                      excluded_selector=css_exclusions,
-                                                      only_text = False, 
-                                                      remove_forms = True, 
-                                                      remove_consent_popups = True, 
-                                                      word_count_threshold = 10)
-
-    async with AsyncWebCrawler(config=browser_cfg) as crawler:
-        """Crawls webpage and extracts content"""
-        # Run the crawler on a URL
-        result: list[CrawlResult] = await crawler.arun(url="https://en.wikipedia.org/wiki/Among_Us")
-        
-        print("-- NO CONFIG --")
-        print(f"[PARSER]: Original HTML file length (in characters): {len(result[0].html)}")
-        print(f"[PARSER] Successfully parsed article titled '{result[0].metadata.get('title')}' for a total of {len(result[0].markdown.raw_markdown)} characters.")
-                                    
-        filtered_result : list[CrawlResult] = await crawler.arun(url="https://en.wikipedia.org/wiki/Among_Us", config=crawler_cfg)
-        soup = BeautifulSoup(filtered_result[0].html, 'html.parser')
-        h1_elem = soup.find('h1', id='firstHeading')
-        title: str = h1_elem.get_text(strip=True) if h1_elem else 'Unknown title'
-
-        result_data: str = filtered_result[0].markdown.fit_markdown # change to raw_markdown if not using PruningContentFilter()
-        body_length = len(result_data)
-
-        print("-- CUSTOM CONFIG --")
-        print(f"[PARSER] Successfully parsed article titled '{title}' for a total of {body_length} characters.")
-        # print(len(filtered_result[0].markdown.fit_markdown))
-        # print(len(filtered_result[0].markdown.raw_markdown)) <--- unfiltered
-
-        generated_tokens: list[str] = cleanup_and_get_tokens(filtered_result[0].markdown.fit_markdown) # change to raw_markdown if not using PruningContentFilter()
-
-        with open('outputs/custom_c.md', 'w', encoding='UTF-8') as fout:
-             fout.write(str(generated_tokens)) # change to raw_markdown if not using PruningContentFilter()
-             # fout.write(filtered_result[0].markdown.fit_markdown)
-
-def cleanup_and_get_tokens(md: str) -> list[str]:
-    '''Cleans up the markdown and returns tokens'''
-    punctuation_remover: dict[int, int | None] = str.maketrans('', '', string.punctuation)
-    to_remove: list[str] = ["## See also", "## Notes", "## References", "## Voci correlate", "## Note", "## Bibliografia"]
-    for elem in to_remove:
-        index_found = md.find(elem)
-        if (index_found != -1):
-            md = md[:index_found] # delete whatever follows since we have no need for it
-    md = re.sub(r'\[[a-zA-Z0-9]+\]', '', md) # remove markdown tags [1], ...
-    md = re.sub(r'[^\w\s]', ' ', md) # further remove markdown
-    clean_str: str = md.translate(punctuation_remover).strip().lower()
-    tokens: list[str] = clean_str.split()
-    return tokens 
+    if (WebParser.DEBUG):
+        with open('src/outputs/last_parse.md', 'w', encoding='UTF-8') as fout: # NOTE: execute from minerva-web-parser dir
+            fout.write(data.get("parsed_text")) 
 
 if __name__ == '__main__':
     # Run the async main function
---
+++ src/parser/WebParser.py
@@ -0,0 +1,90 @@
+from bs4 import BeautifulSoup
+from crawl4ai import AsyncWebCrawler, CrawlResult, CrawlerRunConfig, DefaultMarkdownGenerator, PruningContentFilter, CacheMode, BrowserConfig
+import string
+import regex as re
+
+class WebParser:
+
+    DEBUG: bool = True # print debug messages
+
+    CSS_EXCLUSIONS: str = '''
+    #mw-head, #mw-panel, #footer, #vector-main-menu, .mw-content-subtitle,
+    .vector-header-container, .vector-column-start, .shortdescription, 
+    .vector-sticky-header, .mw-footer, .vector-sitenotice-container, 
+    .reflist, .refbegin, .mw-references-wrap, .infobox, .mw-file-description,
+    .thumb, .mw-editsection, .navbox, .side-box, .hatnote[role="complementary"], 
+    .floatright''' # add .wikitable if too much useless data is parsed. TODO: modify to include only Italian exclusions
+    
+    TARGETS: list[str] = ['.mw-parser-output']
+    TAG_EXCLUSIONS: list[str] = ['nav', 'footer', 'aside', 'script', 'style', 'noscript', 'header', 'figure']
+    MARKDOWN_EXCLUSIONS: list[str] = ["## See also", "## Notes", "## References", "## Voci correlate", "## Note", "## Bibliografia"]
+    WORD_COUNT_THRESHOLD: int = 10
+
+    MARKDOWN_GEN_OPTIONS: dict[str, bool] = {
+        'ignore_images': True, 
+        'escape_html': True, 
+        'ignore_links': True 
+    }
+
+    def __init__(self):
+        self.browser_cfg : BrowserConfig = BrowserConfig(headless=True)
+        self.crawler_cfg : CrawlerRunConfig = CrawlerRunConfig(target_elements=WebParser.TARGETS, excluded_tags=WebParser.TAG_EXCLUSIONS, 
+                                                               markdown_generator= DefaultMarkdownGenerator(PruningContentFilter(), options=WebParser.MARKDOWN_GEN_OPTIONS),
+                                                               excluded_selector=WebParser.CSS_EXCLUSIONS,
+                                                               only_text = False, 
+                                                               remove_forms = True, 
+                                                               remove_consent_popups = True, 
+                                                               word_count_threshold = WebParser.WORD_COUNT_THRESHOLD)
+
+    def __cleanup_and_get_tokens(self, md: str) -> str: # change back to list[str] when done testing
+        '''Cleans up the markdown and returns cleaned markdown string'''
+        punctuation_remover: dict[int, int | None] = str.maketrans('', '', string.punctuation)
+        to_remove: list[str] = WebParser.MARKDOWN_EXCLUSIONS
+        for elem in to_remove:
+            index_found = md.find(elem)
+            if (index_found != -1):
+                md = md[:index_found] # delete whatever follows since we have no need for it
+        '''
+        md = re.sub(r'\[[a-zA-Z0-9]+\]', '', md) # remove markdown tags [1], ...
+        md = re.sub(r'[^\w\s]', ' ', md) # further remove markdown (is this necessary?)
+        clean_str: str = md.translate(punctuation_remover).strip().lower()
+        tokens: list[str] = clean_str.split()
+        return tokens
+        '''
+        return md
+        
+    async def parse_url(self, url: str) -> dict[str, str]:
+        """Crawls webpage and extracts content"""
+        async with AsyncWebCrawler(config=self.browser_cfg) as crawler:
+        # Run the crawler on a URL
+            result: list[CrawlResult] = await crawler.arun(url)
+            
+            if (WebParser.DEBUG):
+                print(f"[PARSER]: Original HTML file length (in characters): {len(result[0].html)}")
+                                        
+            filtered_result : list[CrawlResult] = await crawler.arun(url, config = self.crawler_cfg)
+            soup = BeautifulSoup(filtered_result[0].html, 'html.parser')
+            h1_elem = soup.find('h1', id='firstHeading')
+            title: str = h1_elem.get_text(strip=True) if h1_elem else 'Unknown title'
+
+            page_markdown: str = self.__cleanup_and_get_tokens(filtered_result[0].markdown.fit_markdown) # change to raw_markdown if not using PruningContentFilter()
+            body_length = len(page_markdown)
+
+            if (WebParser.DEBUG):
+                print(f"[PARSER] Successfully parsed article titled '{title}' for a total of {body_length} characters.")
+
+            # generated_tokens: list[str] = self.__cleanup_and_get_tokens(filtered_result[0].markdown.fit_markdown) # change to raw_markdown if not using PruningContentFilter()
+            clean_html: str = filtered_result[0].cleaned_html # pure HTML (no scripts, no CSS)
+
+            ret: dict[str, str] = {
+                "url": url,
+                "domain": "sus",
+                "title": title,
+                "html_text": clean_html,
+                "parsed_text": page_markdown # QUESTION: do we also want links? 
+            }
+
+            return ret
+
+
+    
